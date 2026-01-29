40ac3329f134d9bab820f670864f052c5bb6a412
Adrian-Goulart <adrianx2007@hotmail.com>
2026-01-29 18:45:50 -0300

    ResourceBundles

---
+++ src/praticandoJava/aula/FresourcesBundle/Test/ResourceBundleTest01.java
@@ -0,0 +1,26 @@
+package praticandoJava.aula.FresourcesBundle.Test;
+
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTest01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("pt", "BR"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        bundle = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+    }
+}
+
+// Locale("ja", "JP");
+// messages_ja_JP.properties
+// messages_ja.properties
+// messages_pt_BR.properties
+// messages_pt.properties
+// messages
