63e8cad7af1fc90ef3c28d779a8d99f8fe3d74ac
Adrian-Goulart <adrianx2007@hotmail.com>
2026-01-30 19:14:18 -0300

    Meta caractere Range

---
+++ src/praticandoJava/aula/Gregex/test/PatternMatcherTest03.java
@@ -0,0 +1,29 @@
+package praticandoJava.aula.Gregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+
+        // \d --> Todos os dígitos
+        // \D --> Tudo o que não é dígito
+        // \s --> Espaços em branco (\t \n \f \r são considerados espaços em branco)
+        // \S --> Todos menos os espaços em branco
+        // \w --> a-z A-Z, dígitos e _
+        // \W --> Tudo que não pertence ao \w
+
+        String regex = "0[xX][\\w]";
+        String texto = "12 0x 0x 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto:  " + texto);
+        System.out.println("Índice: 0123456789");
+        System.out.println("Regex: " + regex);
+        System.out.println("Posições encontradas:");
+
+        while (matcher.find()) {
+            System.out.println(matcher.start() + " " + matcher.group());
+        }
+    }
+}
