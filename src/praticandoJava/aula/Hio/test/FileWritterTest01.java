36ba5ee6e861fd63c0e62d8915401f1531e6b8f3
Adrian-Goulart <adrianx2007@hotmail.com>
2026-02-01 13:05:56 -0300

    FileWritter

---
+++ src/praticandoJava/aula/Hio/test/FileWritterTest01.java
@@ -0,0 +1,19 @@
+package praticandoJava.aula.Hio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWritterTest01  {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+
+        try (FileWriter fw = new FileWriter(file, true)){
+            fw.write("Os TOP: Yorushika, Zutomayo, Ado, Yoasobi\n");
+            fw.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
