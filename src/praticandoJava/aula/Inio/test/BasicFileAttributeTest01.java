43f3adc43a5845157f65938cc3faac4bb2687685
Adrian-Goulart <adrianx2007@hotmail.com>
2026-02-03 16:46:55 -0300

    Coisas que esqueci de adicionar antes e Data de Modificação de arquivo

---
+++ src/praticandoJava/aula/Inio/test/BasicFileAttributeTest01.java
@@ -0,0 +1,28 @@
+package praticandoJava.aula.Inio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class BasicFileAttributeTest01 {
+    public static void main(String[] args) throws IOException {
+        LocalDateTime date = LocalDateTime.now().minusDays(30);
+        File file = new File("pasta/subpasta/test.txt");
+        boolean isCreated = file.createNewFile();
+        boolean isModified = file.setLastModified(date.toInstant(ZoneOffset.UTC).toEpochMilli());
+
+        Path path = Paths.get("pasta/subpasta/test_Path.txt");
+        Files.createFile(path);
+        FileTime fileTime = FileTime.from(date.toInstant(ZoneOffset.UTC));
+        Files.setLastModifiedTime(path, fileTime);
+
+        System.out.println(Files.isWritable(path));
+        System.out.println(Files.isReadable(path));
+        System.out.println(Files.isExecutable(path));
+    }
+}
