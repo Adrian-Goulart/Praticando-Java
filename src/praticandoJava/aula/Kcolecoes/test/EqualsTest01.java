606985ac9eed28ccb83e2fe130aae805277bda17
Adrian-Goulart <adrianx2007@hotmail.com>
2026-02-09 18:39:04 -0300

    Coleções comparação

--- src/praticandoJava/aula/Kcolecoes/test/EqualsTest01.java
+++ src/praticandoJava/aula/Kcolecoes/test/EqualsTest01.java
@@ -4,8 +4,8 @@ import praticandoJava.aula.Kcolecoes.dominio.Smartphone;
 
 public class EqualsTest01 {
     public static void main(String[] args) {
-        Smartphone s1 = new Smartphone("2578", "ABCDE");
-        Smartphone s2 = new Smartphone("2578", "ABCDE");
+        Smartphone s1 = new Smartphone("2578", "Samsung");
+        Smartphone s2 = new Smartphone("2578", "Outro");
         System.out.println(s1.equals(s2));
     }
 }
